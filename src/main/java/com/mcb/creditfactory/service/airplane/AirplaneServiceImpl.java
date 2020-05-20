9c6c6b3ae9b1d4ab819b5ee04ba872cbce96b88f
IveriNarozashvili <geobmw91>
2020-05-20 15:49:14 +0300

    Initial commit

---
+++ src/main/java/com/mcb/creditfactory/service/airplane/AirplaneServiceImpl.java
@@ -0,0 +1,65 @@
+package com.mcb.creditfactory.service.airplane;
+
+import com.mcb.creditfactory.dto.AirplaneDto;
+import com.mcb.creditfactory.external.ExternalApproveService;
+import com.mcb.creditfactory.model.Airplane;
+import com.mcb.creditfactory.repository.AirplaneRepository;
+import com.mcb.creditfactory.service.car.CarAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
+public class AirplaneServiceImpl implements AirplaneService {
+    @Autowired
+    private ExternalApproveService approveService;
+
+    @Autowired
+    private AirplaneRepository airplaneRepository;
+
+    @Override
+    public boolean approve(AirplaneDto dto) {
+        return approveService.approve(new AirplaneAdapter() == 0;
+    }
+
+    @Override
+    public Airplane save(Airplane airplane) {
+        return airplaneRepository.save(airplane);
+    }
+
+    @Override
+    public Optional<Airplane> load(Long id) {
+        return AirplaneRepository.findById(id);
+    }
+
+    @Override
+    public Airplane fromDto(AirplaneDto dto) {
+        return new Airplane(
+                dto.getId(),
+                dto.getBrand(),
+                dto.getModel(),
+                dto.getManufactures(),
+                dto.getYear(),
+                dto.getFuelCapacity(),
+                dto.getSeats()
+        );
+    }
+
+    @Override
+    public AirplaneDto toDTO(Airplane airplane) {
+        return new AirplaneDto(
+                airplane.getId(),
+                airplane.getBrand(),
+                airplane.getModel(),
+                airplane.getManufacturer(),
+                airplane.getYear(),
+                airplane.getFuelCapacity(),
+                airplane.getSeats()
+        );
+    }
+
+    @Override
+    public Long getId(Airplane airplane) {
+        return airplane.getId();
+    }
+}
+
+
